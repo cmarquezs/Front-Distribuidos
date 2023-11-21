24effb216f17e96839ffec509467c02f6969cffb
Cristian Marquez <mcristiansaid@gmail.com>
2023-11-21 09:29:17 -0500

    code

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+
+// Función para cargar el contenido del footer desde un archivo externo
+function loadHeaderContent() {
+    fetch('view-header.html')
+        .then(response => response.text())
+        .then(data => {
+            header.innerHTML = data;
+        })
+        .catch(error => console.error('Error fetching footer content:', error));
+}
+
+loadHeaderContent();
